d764bc9cdb0a59f8ddbfa034addb1d93ce124876
Ecturing <ecturing@vip.qq.com>
2022-09-08 14:28:21 +0800

    update

--- wezterm.lua
+++ wezterm.lua
@@ -1,13 +1,21 @@
 local wezterm = require 'wezterm'
-if wezterm.target_triple == '' then
-  default_prog={'C://ProgramFiles//PowerShell//7'}
+local prog
+local font
+
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  prog={'C:/Program Files/PowerShell/7/pwsh.exe'}
+  font='JetBrains Mono'  
+end
+if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
+  prog={'/usr/bin/zsh'}
+  font='JetBrains Mono'  
 end
 return {
+  default_prog=prog,
   window_background_opacity = 0.8,
   tab_bar_at_bottom = true,
   font = wezterm.font_with_fallback {
-    'MesloLGS Nerd Font',
-    'JetBrains Mono',
+    font,
   },
   font_size=11.0,
   color_scheme ="Ayu Mirage",
