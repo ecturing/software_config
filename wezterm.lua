d9ece78e0962450697f43109f677428ca1591e11
Ecturing <ecturing@vip.qq.com>
2022-11-27 09:55:51 +0800

    Update wezterm.lua
    update

--- wezterm.lua
+++ wezterm.lua
@@ -3,7 +3,7 @@ local prog
 local font
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  prog={'C:/Program Files/PowerShell/7/pwsh.exe'}
+  prog={'C:/Users/32145/AppData/Local/Microsoft/WindowsApps/Microsoft.PowerShell_8wekyb3d8bbwe/pwsh.exe'}
   font='JetBrains Mono'  
 end
 if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
@@ -11,12 +11,54 @@ if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
   font='JetBrains Mono'  
 end
 return {
+  enable_tab_bar=false,
   default_prog=prog,
-  window_background_opacity = 0.8,
+  window_background_opacity = 1,
   tab_bar_at_bottom = true,
   font = wezterm.font_with_fallback {
     font,
   },
   font_size=11.0,
   color_scheme ="Ayu Mirage",
-}
+  keys = {
+          { 
+	   key = 'RightArrow',mods = 'CTRL',
+           action = wezterm.action.SplitHorizontal {  domain = 'CurrentPaneDomain'},
+   	},
+	 {
+	key = 'DownArrow',mods = 'CTRL',
+	action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain'},
+	}
+    },
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
