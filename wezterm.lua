6672ac92176e2d1f7bdf0a61b4d4565ec7549301
Ecturing <42050852+ecturing@users.noreply.github.com>
2025-09-05 15:45:38 +0800

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,38 +1,45 @@
 local wezterm = require 'wezterm'
 local prog
-local font
+
+local config = {}
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   prog = { 'pwsh.exe' }
-  font = 'JetBrains Mono'
 end
+
 if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
   prog = { 'zsh' }
-  font = 'JetBrains Mono'
 end
-return {
-  enable_tab_bar = false,
-  default_prog = prog,
-  window_background_opacity = 1,
-  tab_bar_at_bottom = true,
-  font = wezterm.font_with_fallback {
-    font,
-  },
-  font_size = 11.0,
-  color_scheme = "Nord (base16)",
-  keys = {
-    {
-      key = 'RightArrow', mods = 'CTRL',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = 'DownArrow', mods = 'CTRL',
-      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = 'D',
-      mods = 'CTRL',
-      action = wezterm.action.CloseCurrentPane { confirm = true },
-    }
-  },
+
+config.window_frame = {
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
 }
+
+config.font = wezterm.font('JetBrains Mono')
+
+config.animation_fps = 60
+config.default_prog = prog
+config.window_background_opacity = 1
+config.font_size = 12.0
+config.color_scheme = "Belge (terminal.sexy)"
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.keys = {
+  {
+    key = 'RightArrow',
+    mods = 'CTRL',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'DownArrow',
+    mods = 'CTRL',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'D',
+    mods = 'CTRL',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  }
+}
+
+return config
